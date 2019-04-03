2bcbd66705d2c4319063c11aa200ec9eb561fb63
PerIvan <1and130699@gmail.com>
2019-04-04 01:12:49 +0300

    "ddz "

---
+++ ddz-timp/ddz.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Ivan on 04.04.2019.
+//
+
--- task2-3/test.cpp
+++ task2-3/test.cpp
@@ -4,9 +4,9 @@
 #include <string>
 #include "C:\Users\Ivan\Desktop\catch.hpp"
 #include <ctime>
-
+#include <chrono>
 using namespace std;
-
+using namespace std::chrono;
 TEST_CASE("1", "R-K"){
     vector<int> vector1;
     vector1.push_back(Rabin_Karp_Matcher("abcabaabcabac", "abaa", 234, 7));
@@ -32,4 +32,29 @@ TEST_CASE("2", "R-K - N"){
     vector1.push_back(Rabin_Karp_Matcher(s1, s2, 31, 31));
     vector2.push_back(Naive_String_Matcher(s1, s2));
     REQUIRE(vector1 == vector2);
+}
+
+TEST_CASE("TEST 3", "[tag3]"){
+    string s1;
+    s1.resize(500);
+    for(int i=0; i!=s1.size(); ++i){
+        s1[i] = (char)(21 + rand()%234);
+    }
+    string s2;
+    s2.resize(3);
+    for(int i=0; i!=s2.size(); ++i){
+        s2[i] = (char)( rand()% 234 +21);
+    }
+
+    auto start1 = steady_clock::now();
+    Rabin_Karp_Matcher(s1, s2, 1, 1000);
+    auto end1 = steady_clock::now();
+    auto elapsed1 = duration_cast<nanoseconds>(end1 - start1);
+
+    auto start2 = steady_clock::now();
+    Naive_String_Matcher(s1, s2);
+    auto end2 = steady_clock::now();
+    auto elapsed2 = duration_cast<nanoseconds>(end2 - start2);
+
+    REQUIRE(elapsed1<elapsed2);
 }
