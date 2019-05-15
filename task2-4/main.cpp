1f1789cfd230baf1e4ae22a45290b1ba6e9d7893
PerIvan <1and130699@gmail.com>
2019-05-15 14:47:59 +0300

    15.05.19

--- task2-4/main.cpp
+++ task2-4/main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 #include <vector>
-
+#include "task2-4.h"
+#include <fstream>
 using namespace std;
 
 int main() {
-    vector<int> vecInt;
-
+    vector<int> vec_int;
+    vector<string> vec_str;
+    fstream in;
+    in.open("C:/Users/Ivan/CLionProjects/task2-4/digits.txt");
+    if(in.is_open()){
+        string buf;
+        while (!in.eof()){
+            in >>buf;
+            vec_int.push_back(stoi(buf.c_str()));
+            vec_str.push_back(buf);
+        }
+        vector<string> true_vec_str = radix_sorter(vec_str);
+        vector<int> true_vec_int = radix_sorter(vec_int);
+        for (auto i: vec_str)
+            cout << i << endl;
+    }else{
+        cout << "File isn't open" << endl;
+    }
+    return 0;
 }
--- task2-4/task2-4.h
+++ task2-4/task2-4.h
@@ -1,7 +1,123 @@
+//9. Алгоритм сортировки «поразрядная» (MSD).
 
-#ifndef TASK2_4_TASK2_4_H
-#define TASK2_4_TASK2_4_H
+#ifndef TIMP_TASK2_4_H
+#define TIMP_TASK2_4_H
+#endif //TIMP_TASK2_4_H
+#include <string>
+#include <vector>
+#include <map>
+#include <deque>
+#include <cmath>
+#include <ctime>
+#include <sstream>
 
-#endif //TASK2_4_TASK2_4_H
+using namespace std;
 
-int MSD();
+vector<int> radix_sort(vector<int> &vec_int, int d) {
+    for(int i = d; i >=1; i--) {
+        vector<deque<int>> vec_count(20);
+        for (int t = 0; t < vec_int.size(); t++){
+            int j = vec_int[t]/pow(10, i);
+            j =j % (int) pow(10, i-1);
+            vec_count[j+10].push_front(vec_int[t]);
+        }
+        vector<int> vec_new;
+        for(auto& deq: vec_count)
+            while(!deq.empty()){
+                vec_new.emplace_back(deq.back());
+                deq.pop_back();
+            }
+        vec_int = vec_new;
+    }
+    return vec_int;
+}
+
+int get_discharge(int digit) {
+    stringstream tmp;
+    string str;
+    tmp << abs(digit);
+    tmp >> str;
+    return str.size();
+}
+
+int get_max_length(vector<string> &vec_str){
+    int max_length=0;
+    for(auto i: vec_str){
+        if(i.size()>max_length)
+            max_length=i.size();
+    }
+    return max_length;
+}
+vector<vector<int>> prelim_sort(vector<int> &vecInt){
+    vector<vector<int>> prelimVec(10); //вектор очередей, содержащих числа одинакового разряда
+    for(auto i: vecInt)
+        prelimVec[get_discharge(i)].push_back(i);
+    for(auto i: prelimVec)
+    i.shrink_to_fit();
+    prelimVec.shrink_to_fit();
+    return prelimVec;
+}
+
+vector<int> radix_sorter(vector<int> &vec_int) {
+    vector<vector<int>> mod_vec = prelim_sort(vec_int);
+    vector<int> vec_new;
+    vector<int> vec_temp;
+    for(int s=1; s!=mod_vec.size(); s++) {
+            vec_temp = radix_sort(mod_vec[s], 10);
+        for(auto &i: vec_temp)
+            vec_new.emplace_back(i);
+    }
+    vec_int.clear();
+    vec_int=vec_new;
+    return vec_int;
+}
+
+
+vector<string> radix_sort(vector<string> &vec_str) {
+    for(auto &i: vec_str){
+        string reverse;
+        copy(i.rbegin(), i.rend(), back_inserter(reverse));
+        for(int j=0; j<get_max_length(vec_str)-i.size(); j++)
+            reverse.push_back(' ');
+        i.clear();
+        copy(reverse.rbegin(), reverse.rend(), back_inserter(i));
+        cout << i << '|';
+    }
+    cout << endl;
+    for(int i = 0; i < get_max_length(vec_str); i++) {
+        map<int, deque<string>> mapCount;
+        for (int t = 0; t < vec_str.size(); t++) {
+            int j = vec_str[t][i];
+            mapCount[j].push_front(vec_str[t]);
+        }
+        vector<string> vecNew;
+        for(auto& it : mapCount)
+            while(!it.second.empty()) {
+                vecNew.emplace_back(it.second.back());
+                it.second.pop_back();
+            }
+        vec_str = vecNew;
+    }
+    return vec_str;
+}
+
+map<int, vector<string>> prelim_sort(vector<string> &vec_str){
+    map<int, vector<string>> prelimVec;
+    for(auto i: vec_str)
+        prelimVec[i.size()].push_back(i);
+    return prelimVec;
+}
+
+vector<string> radix_sorter(vector<string> &vec_str) {
+map<int, vector<string>> mod_map = prelim_sort(vec_str);
+    vector<string> vec_new;
+    vector<string> vec_temp;
+    for(auto &i : mod_map) {
+        vec_temp = radix_sort(i.second);
+        for(auto &i: vec_temp)
+            vec_new.emplace_back(i);
+    }
+    vec_str.clear();
+    vec_str=vec_new;
+    return vec_str;
+}
